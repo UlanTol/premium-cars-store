d26578aa6db17e3c3bb83e922ebb0163c3b1794c
Rumidich <rumidich@gmail.com>
2022-07-05 15:36:40 +0600

    some changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { TextField } from "@mui/material";
 import { Input } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
@@ -8,21 +7,22 @@ import "../Footer/style.css";
 const Footer = () => {
   const { loginWithRedirect } = useAuth0();
   const navigate = useNavigate();
+
   return (
     <footer>
       <div
         style={{ marginTop: "90px", borderTop: "2px solid white" }}
-        class="f-item-con">
-        <div class="app-info">
-          <span class="app-name">
-            <span class="app-initial">P</span>remium Cars
+        className="f-item-con">
+        <div className="app-info">
+          <span className="app-name">
+            <span className="app-initial">P</span>remium Cars
           </span>
           <p>
             <strong> Buy your best car</strong> enjoy <strong></strong> them
           </p>
         </div>
-        <div class="useful-links">
-          <div class="footer-title">Useful Links</div>
+        <div className="useful-links">
+          <div className="footer-title">Useful Links</div>
           <ul>
             <li onClick={() => navigate("/")}>Main</li>
             <li onClick={() => navigate("/cars")}>Shop</li>
@@ -35,27 +35,27 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div class="help-sec">
-          <div class="footer-title">Help</div>
+        <div className="help-sec">
+          <div className="footer-title">Help</div>
           <ul>
             <li>Help Me</li>
             <li>Feedback</li>
             <li>Report a Issue / Bug</li>
           </ul>
         </div>
-        <div class="g-i-t">
-          <div class="footer-title">Get in Touch</div>
-          <form action="/" method="post" class="space-y-2">
+        <div className="g-i-t">
+          <div className="footer-title">Get in Touch</div>
+          <form action="/" method="post" className="space-y-2">
             <Input placeholder="Name" />
             <Input placeholder="Email" />
             <Input placeholder="Message" />
-            <button type="submit" class="f-btn">
+            <button type="submit" className="f-btn">
               Submit
             </button>
           </form>
         </div>
       </div>
-      <div class="cr-con">Copyright &copy; 2022 | Made by Rawnge</div>
+      <div className="cr-con">Copyright &copy; 2022 | Made by Rawnge</div>
     </footer>
   );
 };
